89d09a91486bf216ed05cbb70a58345104ffee3a
Samuel Sinambela <sinambelasam03@gmail.com>
2020-04-29 20:10:23 +0700

    [touch.c] done

--- touch.c
+++ touch.c
@@ -2,16 +2,33 @@
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
-
+#include "date.h"
+#include "fs.h"
 int main (int argc, char *argv[]){
     int file_input;
+    char buf[8192];
+    //struct stat st;
     if(argc<2){
        printf(1,"touch : Missing one argument...\n"); 
        exit();
     }
     for(int i=1;i<argc;i++){
-        if((file_input=open(argv[i],O_CREATE | O_RDWR)) < 0 ){
-            printf(1,"touch : Error creating file %s\n",argv[i]);
+        if((file_input=open(argv[i],O_RDWR)) < 0 ){
+            file_input=open(argv[i],O_CREATE |O_RDWR);
+            if(file_input<0)
+                printf(1,"touch : Error creating file %s\n",argv[i]);
+        }
+        else{
+            read(file_input, buf,sizeof(buf));
+            close(file_input);
+            unlink(argv[i]);
+            
+            if((file_input=open(argv[i],O_RDWR|O_CREATE)) < 0 ){
+                printf(1,"touch : Error creating file %s\n",argv[i]);
+            }
+            else{
+                write(file_input,buf,sizeof(buf));
+            }
         }
         close(file_input);
     }
